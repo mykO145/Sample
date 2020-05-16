556ced5caae0627ab8b089a18d482179dbcd9e15
mayuko <mayuko@192.168.1.11>
2020-05-16 23:03:13 +0900

    commit test3

--- src/Foods.java
+++ src/Foods.java
@@ -1,7 +1,7 @@
 
 public class Foods {
 	public static void main(String[] args) {
-		System.out.println("好きな食べ物はかき氷です。");
-		System.out.println("なぜなら、夏を感じるからです。");
+		System.out.println("好きな食べ物はキムチです。");
+		System.out.println("なぜなら、辛くて美味しいからです。");
 	}
 }
