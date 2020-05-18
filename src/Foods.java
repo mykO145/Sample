d37bee54df7060e88e0cdf46ba4a43ced1582c87
suzukirisa <suzukirisa@192.168.2.104>
2020-05-18 18:07:50 +0900

    コミット

--- src/Foods.java
+++ src/Foods.java
@@ -3,5 +3,7 @@ public class Foods {
 	public static void main(String[] args) {
 		System.out.println("好きな食べ物はキムチです。");
 		System.out.println("なぜなら、辛くて美味しいからです。");
+		System.out.println("キムチは、白菜などの野菜と、塩・唐辛子、魚介塩辛、ニンニクなどを主に使用した漬物です。");
+				
 	}
 }
